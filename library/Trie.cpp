4e67982152baa421b5857e0a7be2f047ecf56be9
unknown <shubhampandeybhu21@gmail.com>
2022-07-21 16:10:07 +0530

    updated

--- library/Trie.cpp
+++ library/Trie.cpp
@@ -1,69 +1,69 @@
-struct Node
-{
-  Node*link[26]={nullptr}; 
+#define fo(x,y) for(int i=x;i<y;i++)
+struct Node {
+  Node * link[26] = {
+    nullptr
+  };
   bool flag;
 
-  bool containsKey(char c){ 
-    return link[c-'a']!=nullptr;
+  bool containsKey(char c) {
+    return link[c - 'a'] != nullptr;
   }
 
-  void put(char c, Node*node){
-    link[c-'a']=node;
+  void put(char c, Node * node) {
+    link[c - 'a'] = node;
   }
 
-  Node* get(char c){
-    return link[c-'a'];
+  Node * get(char c) {
+    return link[c - 'a'];
   }
 
-  void setEnd(){
-    flag=true;
+  void setEnd() {
+    flag = true;
   }
 
-  bool isEnd(){
+  bool isEnd() {
     return flag;
   }
 
 };
 
-class Trie{
+class Trie {
 
- Node*root;
+  Node * root;
 
   public:
 
-    Trie(){
-      root=new Node();  
+    Trie() {
+      root = new Node();
     }
 
-  void insert(string word){
-    Node*node=root;
-    fo(0,word.size())
-    {
-      if(!node->containsKey(word[i])){
-        node->put(word[i],new Node);
+  void insert(string word) {
+    Node * node = root;
+    fo(0, word.size()) {
+      if (!node -> containsKey(word[i])) {
+        node -> put(word[i], new Node);
       }
-      node=node->get(word[i]);
+      node = node -> get(word[i]);
     }
-    node->setEnd();
-  }  
-
-  bool search(string word){
-    Node*node=root;
-    fo(0,word.size())
-    {
-      if(!node->containsKey(word[i]))
-      return false;
-      node=node->get(word[i]);  
-    }
-    return node->isEnd();
+    node -> setEnd();
   }
 
-  bool startsWith(string prefix){
-    Node*node=root;
-    fo(0,prefix.size()){
-      if(!node->containsKey(prefix[i]))
-      return false;
-      node=node->get(prefix[i]);   
+  bool search(string word) {
+    Node * node = root;
+    fo(0, word.size()) {
+      if (!node -> containsKey(word[i]))
+        return false;
+      node = node -> get(word[i]);
+    }
+    return node -> isEnd();
+  }
+
+  bool startsWith(string prefix) {
+    Node * node = root;
+    fo(0, prefix.size()) {
+      if (!node -> containsKey(prefix[i]))
+        return false;
+      node = node -> get(prefix[i]);
     }
     return true;
   }
