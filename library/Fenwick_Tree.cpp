ad4f134f20796dc9442158fd91c5e40089b3a2bf
unknown <shubhampandeybhu21@gmail.com>
2023-04-21 11:23:36 +0530

    --updated

--- library/Fenwick_Tree.cpp
+++ library/Fenwick_Tree.cpp
@@ -1,32 +1,32 @@
 class FenwickTree {
 public:
-    vector<int>ft;
+	vector<int>ft;
 
-    FenwickTree(int n) {
-        ft.resize(n);
-    }
+	FenwickTree(int n) {
+		ft.resize(n);
+	}
 
-    void update(int index, int val) {
-        while (index < ft.size()) {
-            ft[index] += val;
-            index += (index & (-index));
-        }
-    }
+	void update(int index, int val) {
+		while (index < ft.size()) {
+			ft[index] += val;
+			index += (index & (-index));
+		}
+	}
 
-    int sum(int index) {
-        int s = 0;
-        while (index) {
-            s += ft[index];
-            index -= (index & (-index));
-        }
-        return s;
-    }
+	int sum(int index) {
+		int s = 0;
+		while (index) {
+			s += ft[index];
+			index -= (index & (-index));
+		}
+		return s;
+	}
 
-    int rangeSum(int l, int r) {
-        return sum(r) - sum(l - 1);
-    }
+	int rangeSum(int l, int r) {
+		return sum(r) - sum(l - 1);
+	}
 
-    void getTreeVector() {
-        debug(ft);
-    }
+	void getTreeVector() {
+		debug(ft);
+	}
 };
